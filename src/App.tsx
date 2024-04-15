0b448de59ff442f1b25755bc3fe18510b4bcfe7b
CESCO <cescovalledev@gmail.com>
2024-04-14 23:08:43 -0600

    implementing dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { IoMdMoon } from "react-icons/io";
+import { Button } from "./components/ui/button";
+import { useEffect } from "react";
+import { useUiStore } from "./hooks/useUiStore";
+
 function App() {
+  const { checkTheme, theme, toggleTheme } = useUiStore();
+
+  useEffect(() => {
+    checkTheme();
+  }, [checkTheme, theme]);
+
   return (
     <>
+      <div className="bg-primary shadow-sm p-4 md:px-8 text-primary-foreground flex justify-between align-middle">
+        <h1 className="text-base md:text-lg font-bold">Where in the world?</h1>
+        <Button
+          onClick={() => toggleTheme()}
+          variant={"ghost"}
+          className="flex gap-1"
+        >
+          <IoMdMoon className="my-auto" />
+          <span className="font-semibold">
+            {theme === "dark" ? "Dark Mode" : "Light Mode"}
+          </span>
+        </Button>
+      </div>
       <div className="bg-background text-background-foreground">
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
