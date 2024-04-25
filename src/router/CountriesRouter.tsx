f224f5411c8992722a2459c843cc615f82d215df
CESCO <cescovalledev@gmail.com>
2024-04-24 23:07:37 -0600

    cleaner code

--- src/router/CountriesRouter.tsx
+++ src/router/CountriesRouter.tsx
@@ -1,48 +1,7 @@
-import API from "@/api/apiServices";
-import { Country } from "@/contracts/types/TCountry";
 import { Home } from "@/views/Home";
+import { loaderHome } from "@/views/Home/loaderHome";
 import { Layout } from "@/views/Layout";
 import { createBrowserRouter } from "react-router-dom";
-
-const filteredCountries = (
-  countries: Country[],
-  countryName?: string | null
-) => {
-  if (!countryName) {
-    return countries;
-  }
-  const re = new RegExp(`${countryName.trim().toLowerCase()}`);
-  return countries.filter((country) => {
-    return (
-      re.test(country.name.common.trim().toLowerCase()) ||
-      re.test(country.name.official.trim().toLowerCase())
-    );
-  });
-};
-
-const loaderHome = async ({ request }) => {
-  const url = new URL(request.url);
-  const country = url.searchParams.get("country");
-  const continent = url.searchParams.get("continent");
-  const params = {
-    country,
-    continent,
-  };
-  try {
-    if (params.continent && params.continent != "all") {
-      const { data } = await API.countries.getCountriesByRegion(
-        params.continent
-      );
-
-      return { countries: filteredCountries(data, country), params };
-    } else {
-      const { data } = await API.countries.getAllCountries();
-      return { countries: filteredCountries(data, country), params };
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
 
 export const CountriesRouter = createBrowserRouter([
   {
