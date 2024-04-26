a925680d1af520a232ef20bfb62cd3c7038671df
CESCO <cescovalledev@gmail.com>
2024-04-25 23:59:50 -0600

    better componetization

---
+++ src/views/Detail/DetailCard.tsx
@@ -0,0 +1,94 @@
+import { Button } from "@/components/ui/button";
+import { Region } from "@/contracts/types/TCountry";
+
+interface Props {
+  card: {
+    nativeName: string[];
+    currencies: string[];
+    languages: string[];
+    flags: {
+      svg: string;
+      alt: string;
+    };
+    commonName: string;
+    population: string;
+    region: Region;
+    subregion: string | undefined;
+    capital: string[];
+    tld: string[] | undefined;
+  };
+  borders: string[];
+}
+
+export const DetailCard = ({ card, borders }: Props) => {
+  const {
+    currencies,
+    languages,
+    nativeName,
+    flags,
+    commonName,
+    population,
+    capital,
+    region,
+    subregion,
+    tld,
+  } = card;
+  return (
+    <div className="grid grid-cols-1 gap-10 mt-10 lg:grid-cols-2">
+      <img
+        className="w-full h-auto col-span-1 mx-auto max-w-96 lg:my-auto"
+        src={flags.svg}
+        alt={`img-${flags.alt}`}
+      />
+      <div className="flex flex-col col-span-1 gap-8 md:my-auto">
+        <h1 className="text-xl font-bold">{commonName}</h1>
+        <div className="flex flex-col gap-2">
+          <p>
+            <span className="font-semibold">Native Name: </span>
+            {nativeName.join(", ")}
+          </p>
+          <p>
+            <span className="font-semibold">Population: </span>
+            {population}
+          </p>
+          <p>
+            <span className="font-semibold">Region: </span>
+            {region}
+          </p>
+          <p>
+            <span className="font-semibold">Subregion: </span>
+            {subregion}
+          </p>
+          <p>
+            <span className="font-semibold">Capital: </span>
+            {capital}
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <p>
+            <span className="font-semibold">Top Level Domain: </span>
+            {tld}
+          </p>
+          <p>
+            <span className="font-semibold">Currencies: </span>
+            {currencies.join(", ")}
+          </p>
+          <p>
+            <span className="font-semibold">Languages: </span>
+            {languages.join(", ")}
+          </p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h2 className="font-bold">Border Countries:</h2>
+          <div className="grid grid-cols-2 gap-4 mt-2 md:grid-cols-3 xl:grid-cols-4">
+            {borders.map((border, index) => (
+              <Button key={index} className="col-span-1">
+                {border}
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/views/Detail/useDetailCard.ts
@@ -0,0 +1,57 @@
+import { CountryDetail } from "@/contracts/types/TCountry";
+import { useEffect, useState } from "react";
+
+export const useDetailCard = (country: CountryDetail) => {
+  const [nativeName, setNativeName] = useState<string[]>([]);
+  const [currenciesArray, setCurrenciesArray] = useState<string[]>([]);
+  const [languages, setLanguages] = useState<string[]>([]);
+
+  useEffect(() => {
+    for (const [, value] of Object.entries(country.name.nativeName)) {
+      setNativeName((prev) =>
+        prev.includes(value.common) ? [...prev] : [...prev, value.common]
+      );
+    }
+  }, [country.name]);
+
+  useEffect(() => {
+    if (country.currencies) {
+      for (const [, value] of Object.entries(country.currencies)) {
+        setCurrenciesArray((prev) =>
+          prev.includes(value.name) ? [...prev] : [...prev, value.name]
+        );
+      }
+    } else {
+      setCurrenciesArray(["Unknown"]);
+    }
+  }, [country.currencies]);
+
+  useEffect(() => {
+    if (country.languages) {
+      for (const [, value] of Object.entries(country.languages)) {
+        setLanguages((prev) =>
+          prev.includes(value) ? [...prev] : [...prev, value]
+        );
+      }
+    } else {
+      setLanguages(["Unknown"]);
+    }
+  }, [country.languages]);
+  return {
+    card: {
+      nativeName,
+      currencies: currenciesArray,
+      languages,
+      flags: {
+        svg: country.flags.svg,
+        alt: country.flags.alt,
+      },
+      commonName: country.name.common,
+      population: new Intl.NumberFormat().format(country.population),
+      region: country.region,
+      subregion: country.subregion,
+      capital: country.capital,
+      tld: country.tld,
+    },
+  };
+};
